ee016fa5d1a17001edbc38a1b8368cf64edc1ed8
pupil <pupil@ivkhk.ee>
2022-04-04 10:51:21 +0300

    update

--- flex/js/code.js
+++ flex/js/code.js
@@ -1,21 +1,29 @@
 let numb = 0;
 let step = 0;
 let count = 5;
-function scroll_left(name) {
+function scroll_right(name) {
     if (step < count - 2) {
         step = step + 1;
     }
-    numb = 25 * step * (-1);
+    numb = 24 * step * (-1);
     console.log(numb);
     document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px"+" "+numb + "vw");
     console.log(document.getElementsByClassName(name)[0].style);
 }
 
-function scroll_right(name) {
+function scroll_left(name) {
     if (step > 0) {
         step = step - 1;
     }
     numb = 25 * step * (-1);
     document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px"+" "+numb + "vw");
     console.log(document.getElementsByClassName(name)[0].style);
+}
+function show(name) {
+    if (document.getElementById(name).style.transform == "scaleY(1)") {
+        document.getElementById(name).style.transform = "scaleY(0)";
+    } else {
+        document.getElementById(name).style.transform = "scaleY(1)";
+    }
+
 }
