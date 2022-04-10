60248549f06a66fd81515d6bab4d8ef85d269999
MiFista <aleksei22891@gmail.com>
2022-04-11 01:49:56 +0300

    progress bar

--- flex/js/code.js
+++ flex/js/code.js
@@ -1,13 +1,14 @@
 let numb = 0;
 let step = 0;
 let count = 5;
+
 function scroll_right(name) {
     if (step < count - 2) {
         step = step + 1;
     }
     numb = 24 * step * (-1);
     console.log(numb);
-    document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px"+" "+numb + "vw");
+    document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px" + " " + numb + "vw");
 }
 
 function scroll_left(name) {
@@ -15,12 +16,31 @@ function scroll_left(name) {
         step = step - 1;
     }
     numb = 24 * step * (-1);
-    document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px"+" "+numb + "vw");
+    document.getElementById(name).style.margin = String("0px" + " 0px" + " 0px" + " " + numb + "vw");
 }
+
 function show(name) {
     if (document.getElementById(name).style.transform == "scaleY(1)") {
         document.getElementById(name).style.transform = "scaleY(0)";
     } else {
         document.getElementById(name).style.transform = "scaleY(1)";
     }
+}
+
+
+function moveto(bool, name) {
+    let page_scroll = JSON.parse(localStorage.getItem("myKey"));
+    console.log(page_scroll)
+    if (bool == 'true' && page_scroll != 0) {
+        window.scroll({
+            top: page_scroll,
+            behavior: 'smooth'
+        });
+    }
+    localStorage.setItem("myKey", JSON.stringify(0));
+}
+
+function coord_scroll() {
+    page_scroll = window.pageYOffset || document.documentElement.scrollTop;
+    localStorage.setItem("myKey", JSON.stringify(page_scroll));
 }
---
+++ flex/js/progress_bar.js
@@ -0,0 +1,10 @@
+setInterval(bar, 10);
+
+function bar() {
+    let h_screen = window.screen.height;
+    let h_document = document.body.scrollHeight;
+    let scrolls = window.pageYOffset || document.documentElement.scrollTop;
+    let progress = (scrolls / (h_document - h_screen)) * 100;
+    document.getElementById("progress_bar").style.width = String(progress) + "%";
+    console.log(String(progress) + "%")
+}
