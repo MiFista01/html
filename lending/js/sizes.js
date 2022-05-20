d0527c0f43f786ff90d65a991759fa37753dd52f
pupil <pupil@ivkhk.ee>
2022-05-20 10:23:02 +0300

    aaaaaaaaaaaaaa

--- lending/js/sizes.js
+++ lending/js/sizes.js
@@ -14,9 +14,34 @@ function size() {
         count += 1;
         sessionStorage.setItem("count", count);
     };
-
     path = ["img/fast.png", "img/prise.png", "img/prof.jpg"];
 
+
+    let plus = document.getElementById("plus").querySelectorAll("figure");
+    for (i = 0; i < plus.length; i++) {
+        plus[i].style.width = String(sessionStorage.getItem("width") * 0.18) + "px";
+    }
+    let prise = document.getElementsByClassName("prise");
+    for (i = 0; i < prise.length; i++) {
+        prise[i].style.width = String(sessionStorage.getItem("width") * 0.2) + "px";
+    }
+    let message_img = document.getElementsByClassName("message_img");
+    path = ["img/author1.jpg", "img/author2.jpg", "img/author3.jpg"];
+    let insert_img = new Image();
+    for (i = 0; i < message_img.length; i++) {
+        insert_img.src = path[i];
+        if (insert_img.width < insert_img.height) {
+            message_img[i].style.backgroundSize = "100%";
+        } else {
+            message_img[i].style.backgroundSize = "auto 100%";
+        }
+        message_img[i].style.width = String(sessionStorage.getItem("width") * 0.08) + "px";
+        message_img[i].style.height = String(sessionStorage.getItem("width") * 0.08) + "px";
+        message_img[i].style.backgroundImage = "url(" + insert_img.src + ")";
+    }
+    let message_button = document.getElementById("button");
+    message_button.style.fontSize = String(Math.round(sessionStorage.getItem("width") * 0.012)) + "px";
+    message_button.style.padding = String(Math.round(sessionStorage.getItem("width") * 0.005)) + "px " + String(Math.round(sessionStorage.getItem("width") * 0.012)) + "px";
     let footer_img = new Image();
     path = "img/home.PNG"
     footer_img.src = path;
@@ -27,16 +52,4 @@ function size() {
         contact_img.style.height = String(Math.round(sessionStorage.getItem("width") * 0.2 / relationship)) + "px";
         contact_img.style.backgroundImage = "url(" + footer_img.src + ")";
     };
-    let mass1 = document.getElementById("plus").querySelectorAll("figure");
-    for (i = 0; i < mass1.length; i++) {
-        mass1[i].style.width = String(sessionStorage.getItem("width") * 0.18) + "px";
-    }
-    let mass2 = document.getElementsByClassName("prise");
-    for (i = 0; i < mass2.length; i++) {
-        mass2[i].style.width = String(sessionStorage.getItem("width") * 0.2) + "px";
-    }
-    let message_button = document.getElementById("button");
-    message_button.style.fontSize = String(Math.round(sessionStorage.getItem("width") * 0.012)) + "px";
-    message_button.style.padding = String(Math.round(sessionStorage.getItem("width") * 0.005)) + "px " + String(Math.round(sessionStorage.getItem("width") * 0.012)) + "px"
-
 }
