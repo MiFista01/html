557d42bf16916a338d2cf26c5aec4d430e9700f1
MiFista01 <aleksei22891@gmail.com>
2022-06-17 13:33:42 +0300

    update

---
+++ final_work/js/mobile.js
@@ -0,0 +1,14 @@
+setInterval(check_device,100);
+let detect = new MobileDetect(window.navigator.userAgent);
+function check_device(){
+    detect = new MobileDetect(window.navigator.userAgent);
+    if(detect.phone()){
+        if(!document.getElementById("head").outerHTML.includes(`<link rel="stylesheet" type="text/css" media="screen" href="css/mobile.css">`)){
+            document.getElementById("head").innerHTML = document.getElementById("head").outerHTML+`<link rel='stylesheet' type='text/css' media='screen' href='css/mobile.css'>`;
+        }    
+    }else{
+        if(document.getElementById("head").outerHTML.includes(`<link rel="stylesheet" type="text/css" media="screen" href="css/mobile.css">`)){
+            document.getElementById("head").innerHTML = document.getElementById("head").outerHTML.split(`<link rel="stylesheet" type="text/css" media="screen" href="css/mobile.css">`).join('');
+        }
+    }
+}
--- final_work/js/show.js
+++ final_work/js/show.js
@@ -2,3 +2,11 @@ document.addEventListener("DOMContentLoaded",add_event);
 function add_event(){
     document.getElementById("menu").addEventListener("click",dmenu_show);
 }
+function dmenu_show(){
+    if(document.getElementById("dmenu").style.transform == "" || document.getElementById("dmenu").style.transform == "scaleY(0)"){
+        document.getElementById("dmenu").style.transform = "scaleY(1)"
+    }else{
+        document.getElementById("dmenu").style.transform = "scaleY(0)"
+    }
+    
+}
